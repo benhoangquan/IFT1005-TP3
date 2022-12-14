5a4961e216b3920d5b4af94d6a068b2d9d3acbd2
benhoangquan <hoangquan.tran410@gmail.com>
2022-12-13 19:52:10 -0500

    Quan's final update: product and productS page are done

--- Fichier de base/assets/js/product.js
+++ Fichier de base/assets/js/product.js
@@ -3,6 +3,7 @@ var productList = [];
 $.urlParam = function(name){
 	var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
 	return results[1] || 0;
+    
 }
 
 function range(start, end) {
@@ -12,7 +13,7 @@ function range(start, end) {
 
 function displayInfoProducts(id){
     index = id - 1; 
-    if (index in range(0, productList.length)){
+    if (index in range(0, productList.length + 1)){
         $("#product-image").attr("src", "./assets/img/" + productList[index].image);
 
         $("#product-name").html(productList[index].name);
@@ -36,11 +37,55 @@ $(document).ready(function () {
         type: "GET",
         url: "./data/products.json",
         datatype: 'json',
-        success: function(xhr){
+        success: function(xhr) {
             productList = xhr; 
+            if (!window.location.href.includes("?id="))
+                window.location.assign("/product.html?id=1"); 
             displayInfoProducts($.urlParam('id')); 
         }
     }); 
 });
 
-// console.log($.urlParam('id'));
+
+function addToLocalStorage(id) {
+    var currentItem = window.localStorage.getItem(productList[id].name); 
+    // create new item if item doesnt exist in local storage
+    if (currentItem == null){
+        var newItem = new Object(); 
+        newItem["id"] = parseInt($.urlParam('id')); 
+        newItem["quantity"] = parseInt($("#product-quantity").val()); 
+        newItem["price"] = parseInt(productList[id].price); 
+        window.localStorage.setItem(productList[id].name, JSON.stringify(newItem));
+    }
+
+    // update current item if it exists
+    else {
+        currentItem = JSON.parse(currentItem);
+        currentItem.quantity += parseInt($("#product-quantity").val()); 
+        window.localStorage.setItem(productList[id].name, JSON.stringify(currentItem))
+    }
+}
+
+function openDialog(message){
+    dialogBox = '<dialog open id ="dialog">' + message + '</dialog>';
+    $("article").append(dialogBox); 
+    $("#dialog").fadeOut(5000,function() {
+        $(this).modal('hide');
+    });
+}
+$(document).ready(function () {
+    var currentUrl = window.location.href; 
+    $("#add-to-cart-btn").click(function () {
+        $.ajax({
+            async: true, 
+            type: "post",
+            url: currentUrl, 
+            datatype: "json",
+            success: function() {
+                var id = $.urlParam('id') - 1; 
+                addToLocalStorage(id);
+                openDialog("Le produit a été ajouté au panier.");
+            }
+        }); 
+    });
+});
